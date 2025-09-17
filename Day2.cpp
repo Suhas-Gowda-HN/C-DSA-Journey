67aede8eba2d981850e8248081704672f78aa97b
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2025-09-17 23:03:31 +0530

    Create Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;             // integer box holding 20
+    double pi = 3.14159;      // decimal number
+    char grade = 'A';         // single character (note single quotes)
+    string name = "Suhas";    // string (sequence of chars, use double quotes)
+    bool isStudent = true;    // true or false
+
+    cout << "Age: " << age << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Name: " << name << endl;
+    cout << "Student? " << isStudent << endl;
+
+    return 0;
+}
+// Practice (Do These Now)
+// Make a variable city and store your city’s name. Print:
+// I live in <city>.
+// Create two integers a and b with any values. Print their sum.
+// Change isStudent to false. What prints?
+
